1786054fc3285eed9436c118d54147a4c1b26b83
Lajos Cseppentő <cseppento@outlook.com>
2017-07-02 20:24:51 +0200

    Update integration test

--- src/integTest/java/com/github/cseppento/gradle/evosuite/EvoSuitePluginIntegTest.java
+++ src/integTest/java/com/github/cseppento/gradle/evosuite/EvoSuitePluginIntegTest.java
@@ -1,5 +1,6 @@
 package com.github.cseppento.gradle.evosuite;
 
+import com.google.common.collect.Sets;
 import org.gradle.api.GradleException;
 import org.gradle.api.Project;
 import org.gradle.api.internal.plugins.PluginApplicationException;
@@ -15,13 +16,15 @@ import org.junit.rules.ExpectedException;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.github.cseppento.gradle.evosuite.testhelper.ExceptionMessageMatcher.messageContainsString;
 import static org.hamcrest.CoreMatchers.isA;
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.hasItems;
+import static org.junit.Assert.*;
 
 public class EvoSuitePluginIntegTest {
     @Rule
@@ -33,6 +36,7 @@ public class EvoSuitePluginIntegTest {
     @Before
     public void setUp() {
         project = ProjectBuilder.builder().withName("test-project").build();
+        project.getRepositories().mavenCentral();
         projectDir = project.getProjectDir().toPath();
     }
 
@@ -47,13 +51,23 @@ public class EvoSuitePluginIntegTest {
         assertEquals(1, sourceSet.getJava().getSrcDirs().size());
         assertEquals(1, sourceSet.getResources().getSrcDirs().size());
 
+        Set<Path> srcDirs = toRelPaths(sourceSet.getAllSource().getSrcDirs());
+        assertEquals(pathSet("src/evoTest/java", "src/evoTest/resources"), srcDirs);
+
         Set<Path> compileCp = toRelPaths(sourceSet.getCompileClasspath().getFiles());
         Set<Path> runtimeCp = toRelPaths(sourceSet.getRuntimeClasspath().getFiles());
 
-        assertEquals(pathSet("build/classes/java/main", "build/resources/main"), compileCp);
-        assertEquals(pathSet(
+        assertThat(compileCp, hasItems(pathArray("build/classes/java/main", "build/resources/main")));
+        assertThat(runtimeCp, hasItems(pathArray(
                 "build/classes/java/main", "build/resources/main",
-                "build/classes/java/evosuiteTest", "build/resources/evosuiteTest"), runtimeCp);
+                "build/classes/java/evosuiteTest", "build/resources/evosuiteTest")));
+
+        Optional<Path> evoSuiteDep = compileCp.stream()
+                .filter(p -> p.getFileName().endsWith("evosuite-client-1.0.5.jar"))
+                .findAny();
+        assertTrue("Missing EvoSuite dependency: " + compileCp, evoSuiteDep.isPresent());
+
+        assertEquals(pathSet("build/classes/java/evosuiteTest", "build/resources/evosuiteTest"), Sets.difference(runtimeCp, compileCp));
     }
 
     @Test
@@ -67,10 +81,17 @@ public class EvoSuitePluginIntegTest {
     }
 
     private Set<Path> toRelPaths(Set<File> files) {
-        return files.stream().map(File::toPath).map(projectDir::relativize).collect(Collectors.toSet());
+        return files.stream()
+                .map(File::toPath)
+                .map(projectDir::relativize)
+                .collect(Collectors.toSet());
     }
 
     private Set<Path> pathSet(String... paths) {
         return Stream.of(paths).map(Paths::get).collect(Collectors.toSet());
+    }
+
+    private Path[] pathArray(String... paths) {
+        return Stream.of(paths).map(Paths::get).toArray(Path[]::new);
     }
 }
